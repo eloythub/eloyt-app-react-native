8e98a03e6677e69dfbcea4b0912ab51f9653c455
Mahan <eng.mahan.hazrati@gmail.com>
2016-09-19 09:31:05 +0700

    add camera

--- app/components/home/record.js
+++ app/components/home/record.js
@@ -6,8 +6,10 @@ import {
   View,
   Text,
   StyleSheet,
+  Dimensions,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Camera from 'react-native-camera';
 
 export default class Record extends Component {
   constructor (props) {
@@ -16,14 +18,40 @@ export default class Record extends Component {
 
   render() {
     return (
-      <View>
-        <Text>
-          Record Page
-        </Text>
+      <View style={style.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={style.preview}
+          aspect={Camera.constants.Aspect.fill}
+          >
+          <Text style={style.capture}>
+            [Capture]
+          </Text>
+        </Camera>
       </View>
     );
   }
 }
 
 const style = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  },
 });
